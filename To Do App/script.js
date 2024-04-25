bb8a688363b9924de170c9e1663775a746cdac9e
jr-boney <85611393+jr-boney@users.noreply.github.com>
2024-04-25 23:37:13 +0530

    Update script.js

--- To Do App/script.js
+++ To Do App/script.js
@@ -34,7 +34,8 @@ taskForm.addEventListener("submit", (e) => {
 
   const dataArrIndex = taskData.findIndex((item) => item.id === currentTask.id);
   const taskObj = {
-    id: `${titleInput.value.toLowerCase().split(' ').join('-')}`,  
+    id: `${titleInput.value.toLowerCase().split(" ").join("-")}-${Date.now()}`,
+    
   };
   console.log(taskObj);
 });
