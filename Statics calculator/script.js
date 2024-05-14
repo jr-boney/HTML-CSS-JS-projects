1d9f96786d2e489adc4bd98bb02d9e6b3a57c720
jr-boney <85611393+jr-boney@users.noreply.github.com>
2024-05-14 23:56:21 +0530

    Update script.js

--- Statics calculator/script.js
+++ Statics calculator/script.js
@@ -1,1 +1,6 @@
-script
+const calculate = () => {
+  const value = document.querySelector("#numbers").value;
+  const array = value.split(/,\s*/g);
+  const numbers = array.map(el => Number(el));
+  const filtered = numbers.filter(el => !Number.isNaN(el));
+}
